3f175f69a2a863a5f0864ba2b2e24fe80e6cedb0
Sung Kim <sungmkim80@gmail.com>
2018-05-23 00:43:16 -0400

    added gatsy graphql source for hacker news API

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,56 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const axios = require('axios')
+const crypto = require('crypto')
 
- // You can delete this file if you're not using it
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators
+
+  const res = await axios.get(
+    `https://hacker-news.firebaseio.com/v0/topstories.json`
+  )
+
+  const getItems = async storyIds => {
+    const itemsPromises = res.data.map(
+      async (storyId, index) =>
+        await axios.get(
+          `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+        )
+    )
+    return Promise.all(itemsPromises)
+  }
+
+  const items = (await getItems(res.data)).map(res => res.data)
+  const findItemById = storyId => {
+    const result = items.find(item => item.id === storyId)
+    console.log(`findItemById(${storyId}) => ${result}`)
+    return result
+  }
+
+  //   console.log(`items`, items)
+
+  res.data.map((storyId, index) => {
+    const storyIdNode = {
+      id: `${index}`,
+      parent: null,
+      internal: {
+        type: `TopStories`,
+      },
+      children: [],
+      storyId: storyId,
+      item: findItemById(storyId),
+    }
+
+    // Get content digest of node. (Required field)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(storyIdNode))
+      .digest(`hex`)
+    // add it to userNode
+    storyIdNode.internal.contentDigest = contentDigest
+
+    // Create node with the gatsby createNode() API
+    createNode(storyIdNode)
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,52 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  const stories = data.allTopStories.edges.map(({ node }, index) => {
+    const { title, score, by, time, type, url } = node.item
+    return (
+      <div key={node.id}>
+        <h6>
+          {type} story {by}
+        </h6>
+        <a href={url}>{title}</a>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+
+      {stories}
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query StoriesQuery {
+    allTopStories {
+      edges {
+        node {
+          id
+          storyId
+          item {
+            id
+            title
+            score
+            by
+            time
+            type
+            url
+          }
+        }
+      }
+    }
+  }
+`
